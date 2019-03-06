7dd7a916879db66a3e8748125c5b5fc28380bd51
gcl1fit2cloud <chenglong.gong@fit2cloud.com>
2019-03-06 17:39:36 +0800

    Modify appsepc.yml

--- src/main/java/com/ctalix/mytest/controller/GreetingController.java
+++ src/main/java/com/ctalix/mytest/controller/GreetingController.java
@@ -1,10 +1,25 @@
 package com.ctalix.mytest.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
- * TODO
+ * Test case .
  *
  * @author <a href="mailto:gcx909109@sina.cn">弓成龙</a>"
  * @create 2019-03-06 08:06
  **/
+
+@RestController
+@RequestMapping(value = "case")
 public class GreetingController {
+
+    @RequestMapping(value = "greeting")
+    @ResponseBody
+    public String greeting() {
+        return "greeting ...";
+    }
+
 }
