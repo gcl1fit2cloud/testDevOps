67a50757bd08664bfb3b2836cfd5e0107128a152
gcl1fit2cloud <chenglong.gong@fit2cloud.com>
2019-03-06 10:01:37 +0800

    Modify pom.xml

---
+++ src/main/java/com/ctalix/mytest/controller/GreetingController.java
@@ -0,0 +1,10 @@
+package com.ctalix.mytest.controller;
+
+/**
+ * TODO
+ *
+ * @author <a href="mailto:gcx909109@sina.cn">弓成龙</a>"
+ * @create 2019-03-06 08:06
+ **/
+public class GreetingController {
+}
